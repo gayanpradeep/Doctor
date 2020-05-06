a17045be2cbd8f7c66809ec85998f1c20e3b17ff
Gayan <gayanpj1996@gmail.com>
2020-05-06 23:11:42 +0530

    first Commnit

---
+++ WebContent/Components/doctor.js
@@ -0,0 +1,197 @@
+$(document).ready(function() 
+{  
+	if ($("#alertSuccess").text().trim() == "")  
+	{   
+		$("#alertSuccess").hide();  
+	}  
+	$("#alertError").hide(); }); 
+ 
+// SAVE ============================================ 
+$(document).on("click", "#btnSave", function(event) 
+{  
+	// Clear alerts---------------------  
+	$("#alertSuccess").text("");  
+	$("#alertSuccess").hide();  
+	$("#alertError").text("");  
+	$("#alertError").hide(); 
+ 
+	// Form validation-------------------  
+	var status = validateDoctorForm();  
+	if (status != true)  
+	{   
+		$("#alertError").text(status);   
+		$("#alertError").show();   
+		return;  
+	} 
+ 
+	// If valid------------------------  
+	var type = ($("#hidDoctorIDSave").val() == "") ? "POST" : "PUT"; 
+	
+	$.ajax( 
+	{  
+		url : "DoctorsAPI",  
+		type : type,  
+		data : $("#formDoctor").serialize(),  
+		dataType : "text",  
+		complete : function(response, status)  
+		{   
+			onDoctorSaveComplete(response.responseText, status);  
+		} 
+	}); 
+}); 
+
+function onDoctorSaveComplete(response, status) 
+{  
+	if (status == "success")  
+	{   
+		var resultSet = JSON.parse(response); 
+
+		if (resultSet.status.trim() == "success")   
+		{    
+			$("#alertSuccess").text("Successfully saved.");    
+			$("#alertSuccess").show(); 
+
+			$("#divDoctorGrid").html(resultSet.data);   
+		} else if (resultSet.status.trim() == "error")   
+		{    
+			$("#alertError").text(resultSet.data);    
+			$("#alertError").show();   
+		} 
+
+	} else if (status == "error")  
+	{   
+		$("#alertError").text("Error while saving.");   
+		$("#alertError").show();  
+	} else  
+	{   
+		$("#alertError").text("Unknown error while saving..");   
+		$("#alertError").show();  
+	} 
+
+	$("#hidDoctorIDSave").val("");  
+	$("#formDoctor")[0].reset(); 
+} 
+ 
+// UPDATE========================================== 
+$(document).on("click", ".btnUpdate", function(event) 
+{     
+	$("#hidDoctorIDSave").val($(this).closest("tr").find('#hidDoctorIDUpdate').val());         
+	$("#fdname").val($(this).closest("tr").find('td:eq(0)').text()); 
+	$("#ldname").val($(this).closest("tr").find('td:eq(1)').text());
+	$("#phone").val($(this).closest("tr").find('td:eq(3)').text());
+	$("#charges").val($(this).closest("tr").find('td:eq(2)').text());  
+	$("#hid").val($(this).closest("tr").find('td:eq(4)').text());
+	$("#uid").val($(this).closest("tr").find('td:eq(5)').text());
+	$("#speciality").val($(this).closest("tr").find('td:eq(6)').text());
+ 
+}); 
+
+//REMOVE===========================================
+$(document).on("click", ".btnRemove", function(event) 
+{  
+	$.ajax(  
+	{   
+		url : "DoctorsAPI",   
+		type : "DELETE",   
+		data : "doctorID=" + $(this).data("doctorid"),   
+		dataType : "text",   
+		complete : function(response, status)   
+		{    
+			onDoctorDeleteComplete(response.responseText, status);   
+		}  
+	}); 
+}); 
+
+function onDoctorDeleteComplete(response, status) 
+{  
+	if (status == "success")  
+	{   
+		var resultSet = JSON.parse(response); 
+
+		if (resultSet.status.trim() == "success")   
+		{    
+			$("#alertSuccess").text("Successfully deleted.");    
+			$("#alertSuccess").show(); 
+		
+			$("#divDoctorGrid").html(resultSet.data);   
+		} else if (resultSet.status.trim() == "error")   
+		{    
+			$("#alertError").text(resultSet.data);    
+			$("#alertError").show();   
+		}
+
+	} else if (status == "error")  
+	{   
+		$("#alertError").text("Error while deleting.");   
+		$("#alertError").show();  
+	} else  
+	{   
+		$("#alertError").text("Unknown error while deleting..");   
+		$("#alertError").show();  
+	}
+}
+ 
+// CLIENT-MODEL========================================================================= 
+function validateDoctorForm() 
+{  
+	// DOCTOR ID  
+	if ($("#doctorID").val().trim() == "")  
+	{   
+		return "Insert Doctor ID.";  
+	} 
+ 
+	// FIRST NAME  
+	if ($("#fdname").val().trim() == "")  
+	{   
+		return "Insert First Name.";  
+	} 
+	
+	// LAST NAME  
+	if ($("#ldname").val().trim() == "")  
+	{   
+		return "Insert Last Name.";  
+	}
+	
+	// MOBILE NUMBER  
+	if ($("#phone").val().trim() == "")  
+	{   
+		return "Insert Mobile Number.";  
+	}
+	
+	//CHARGES-------------------------------  
+	if ($("#charges").val().trim() == "")  
+	{   
+		return "Insert Charges.";  
+	} 
+
+	// is numerical value  
+	var tmpcharges = $("#charges").val().trim();  
+	if (!$.isNumeric(tmpcharges))  
+	{   
+		return "Insert a numerical value for Doctor Price.";  
+	} 
+
+	// convert to decimal price  
+	$("#charges").val(parseFloat(tmpcharges).toFixed(2)); 
+
+	// HOSPITAL ID 
+	if ($("#hid").val().trim() == "")  
+	{   
+		return "Insert Hospital ID.";  
+	} 
+	
+	// USER ID 
+	if ($("#uid").val().trim() == "")  
+	{   
+		return "Insert User ID.";  
+	} 
+	
+	// SPECIALITY 
+	if ($("#speciality").val().trim() == "")  
+	{   
+		return "Insert Your Speciality.";  
+	} 
+	
+
+	return true; 
+}
---
+++ src/model/DoctorsAPI.java
@@ -0,0 +1,120 @@
+package model;
+
+import model.Doctor;
+import java.io.IOException; 
+import java.util.HashMap; 
+import java.util.Map; 
+import java.util.Scanner;
+
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+/**
+ * Servlet implementation class DoctorsAPI
+ */
+@WebServlet("/DoctorsAPI")
+public class DoctorsAPI extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+	Doctor doctorObj = new Doctor();
+	
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public DoctorsAPI() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException 
+	{
+		String output = doctorObj.insertDoctor(      
+				request.getParameter("fdname"),     
+				request.getParameter("ldname"),
+				request.getParameter("phone"),
+				request.getParameter("charges"),
+				request.getParameter("hid"),
+				request.getParameter("uid"),
+				request.getParameter("speciality")); 
+	 
+	 response.getWriter().write(output); 
+		
+		
+	}
+
+	/**
+	 * @see HttpServlet#doPut(HttpServletRequest, HttpServletResponse)
+	 */
+	protected void doPut(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException 
+	{
+		Map paras = getParasMap(request); 
+		 
+		 String output = doctorObj.updateDoctor(paras.get("hidDoctorIDSave").toString(),     
+				      
+				 paras.get("fdname").toString(),
+				 paras.get("ldname").toString(),
+				 paras.get("phone").toString(),
+				 paras.get("charges").toString(),
+				 paras.get("hid").toString(), 
+				 paras.get("uid").toString(),
+				 paras.get("speciality").toString()); 
+		 
+		 response.getWriter().write(output);
+	}
+
+	/**
+	 * @see HttpServlet#doDelete(HttpServletRequest, HttpServletResponse)
+	 */
+	protected void doDelete(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException 
+	{
+		
+		Map paras = getParasMap(request); 
+		 
+		 String output = doctorObj.deleteDoctor(paras.get("doctorID").toString()); 
+		 
+		 response.getWriter().write(output);
+	}
+
+	
+	// Convert request parameters to a Map 
+		private static Map getParasMap(HttpServletRequest request) 
+		{  
+			Map<String, String> map = new HashMap<String, String>();  
+			try  
+			{   
+				Scanner scanner = new Scanner(request.getInputStream(), "UTF-8");   
+				String queryString = scanner.hasNext() ?          
+						scanner.useDelimiter("\\A").next() : "";  
+						scanner.close(); 
+		 
+		  String[] params = queryString.split("&");   
+		  for (String param : params)   
+		  { 
+
+	 
+		   String[] p = param.split("=");    
+		   map.put(p[0], p[1]);   
+		   }  
+		  }  
+			catch (Exception e)  
+			{  
+				
+			}  return map; 
+			
+		}
+}
